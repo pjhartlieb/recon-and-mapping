b7300d77b4c64522979624d8a26771f06ccfd89a
philip hartlieb <philip.hartlieb@gmail.com>
2013-11-11 11:38:59 -0500

    Added error catching for connections made via mech

--- yeyo.pl
+++ yeyo.pl
@@ -13,7 +13,7 @@
 
 # This script will scrape yatedo search results for a specific company
 ## TBD
-## **need** to catch errors using eval.  right now it will shit the bed if mech cannot connect
+## **need** to catch errors using eval.  right now it will shit the bed if mech cannot connect DONE! (I think)
 ## correct link account for additional results pages
 ## write results to file
 ## improve look of output
@@ -92,12 +92,23 @@ help()                   if $help;
 ## retrieve frontpage
 	print "\n";
 	print "[*] Retrieving frontpage for " . $target_site . " \n";
-	$mech->get( $frontpage );
+	#$mech->get( $frontpage );
+	
+	my $connection_1 = eval {						#make connection and catch any "GET" errors
+    	$mech->get( $frontpage );
+    	1
+		};
+	
+	if (! $connection_1) {
+    	print "[*] ERROR. Unable to retrieve frontpage. \n";
+	};
+
 	if ($mech->success) {
 		print "[*] Yatedo appears to be up \n";
 	    } else {
-	        die ("[*] ERROR. Yatedo does not appear to be responding. Exiting \n");
+	        die ("[*] ERROR. Yatedo does not appear to be responding. Exiting \n\n");
 	}
+
 ## sleep
 	my $random_number = int(rand($range));		#generate random integer between 0-20 s
 	print "[*] Sleeping for " . $random_number . " seconds to avoid lockout \n";
@@ -107,24 +118,31 @@ help()                   if $help;
 #submit yatedo search with captured cookies
 ## set form parameters
 	print "[*] Submitting search for " . $keyword . "\n";
-	$mech->submit_form(						#cookies will be automatically included with the request
-        	form_number => 1,
-        	fields => {
-                	q => "companyname:(($keyword))",
-                	c => 'all',
-                	start => 0,
-					p => 1,
-					advs => 0,
-					rlg => 'en',
-                	},
-        	button => 'btn_es');
+	my $connection_2 = eval {
+		$mech->submit_form(						#cookies will be automatically included with the request
+	        	form_number => 1,
+	        	fields => {
+	                	q => "companyname:(($keyword))",
+	                	c => 'all',
+	                	start => 0,
+						p => 1,
+						advs => 0,
+						rlg => 'en',
+	                	},
+	        	button => 'btn_es');
+		1
+		};
+
+		if (! $connection_1) {
+    	print "[*] ERROR. Unable to submit search. \n";
+		};
 
 ##verify and decode content
 	if ($mech->success) {
 		print "[*] Search successful. Content retrieved \n";
         $content = $mech->response()->decoded_content();	
         } else {
-        	die ("[*] ERROR. No content returned. You may want to try a different keyword. Exiting \n");
+        	die ("[*] ERROR. No content returned. You may want to try a different keyword. Exiting \n\n");
     }
         
 ##extract all links for people *and* results pages on the returned page
@@ -151,12 +169,20 @@ help()                   if $help;
 	
 ##retrive page with links and results for all public profiles
 	print "[*] Retrieving cumulative results for " . $keyword . " \n";			#retrieve and verify content
-	$mech->get( $all_in );													
+	my $connection_3 = eval {
+		$mech->get( $all_in );
+		1
+		};
+
+		if (! $connection_1) {
+    	print "[*] ERROR. Unable to retrieve list of public profiles. \n";
+		};	
+
 	if ($mech->success) {
         	my $content_2 = $mech->response()->decoded_content();
         	print "[*] Successful. Content retrieved \n";
         } else {
-        	die ("[*] ERROR. No content returned from " . $all_in . " . Exiting \n");
+        	die ("[*] ERROR. No content returned from " . $all_in . " . Exiting \n\n");
     }
 
 ##parse out links to people *and* links to additional results pages  
@@ -188,71 +214,80 @@ help()                   if $help;
 	print "[*] Harvesting human target data with sleep times between 0 and " . $range . " seconds between records \n";
 	print "\n";
 	for my $uniq_target_url (@uniq_target_url) {
-		$mech->get( $uniq_target_url );
-		if ($mech->success) {
-      
-			my $scraping = $mech-> content;		#response()->decoded_content();
-			
-			#create trees to walk the content of the target pages 
-			my $tree_iter = HTML::Tree->new(); 		#create new tree object
-		
-			$tree_iter->parse($scraping);			#populate tree object with parsed content
-			
-			my $span_first;
-			my ($span_f) = $tree_iter->look_down(	#extract firstname and print as text
-				_tag => "span",
-				class => "given-name firstname",
-				);
-				if(length($span_f) != 0) {
-					$span_first = $span_f->as_text;
-				} else {
-					$span_first = "undef";
-				}
-			
-			my $span_last;
-			my ($span_l) = $tree_iter->look_down(	#extract lastname and print as text
-				_tag => "span",
-				class => "family-name lastname",
-				);
-				if(length($span_f) != 0) {
-					$span_last = $span_l->as_text;
-				} else {
-					$span_last = "undef";
-				}
+		my $connection_4 = eval {
+			$mech->get( $uniq_target_url );
+			1
+			};
 
-			my $span_org;
-			my ($span_o) = $tree_iter->look_down(	#extract organization and print as text
-				_tag => "span",
-				class => "org",
-				);
-				if(defined ($span_o)) {
-					$span_org = $span_o->as_text;
-				} else {
-					$span_org = "undef|past-employer";
-				}
-			
-			my $span_role;
-			my ($span_r) = $tree_iter->look_down(	#extract role and print as text
-				_tag => "span",
-				class => "role",
-				);
-				if(defined ($span_r)) {
-					$span_role = $span_r->as_text;
-				} else {
-					$span_role = "undef|past-role";
-				}
-			if ($span_org =~ m/.*($keyword).*/i) {
-				my $entry="$span_first,$span_last,$span_org,$span_role";
-				push (@humint_targets, $entry);
-			}
-			
-			my $random_number_2 = int(rand($range));
-			sleep($random_number_2);
+		if (! $connection_4 ) {
+    	print "[*] ERROR. Unable to retrieve target data for $uniq_target_url. Moving on. \n";
+		} else {	
 
-			} else {
-        	print ("[*] ERROR. Could not retrieve target data for " . $uniq_target_url . " . Moving on. \n");
-        }
-	}
+			if ($mech->success) {
+	      
+				my $scraping = $mech-> content;		#response()->decoded_content();
+				
+				#create trees to walk the content of the target pages 
+				my $tree_iter = HTML::Tree->new(); 		#create new tree object
+			
+				$tree_iter->parse($scraping);			#populate tree object with parsed content
+				
+				my $span_first;
+				my ($span_f) = $tree_iter->look_down(	#extract firstname and print as text
+					_tag => "span",
+					class => "given-name firstname",
+					);
+					if(length($span_f) != 0) {
+						$span_first = $span_f->as_text;
+					} else {
+						$span_first = "undef";
+					}
+				
+				my $span_last;
+				my ($span_l) = $tree_iter->look_down(	#extract lastname and print as text
+					_tag => "span",
+					class => "family-name lastname",
+					);
+					if(length($span_f) != 0) {
+						$span_last = $span_l->as_text;
+					} else {
+						$span_last = "undef";
+					}
+
+				my $span_org;
+				my ($span_o) = $tree_iter->look_down(	#extract organization and print as text
+					_tag => "span",
+					class => "org",
+					);
+					if(defined ($span_o)) {
+						$span_org = $span_o->as_text;
+					} else {
+						$span_org = "undef|past-employer";
+					}
+				
+				my $span_role;
+				my ($span_r) = $tree_iter->look_down(	#extract role and print as text
+					_tag => "span",
+					class => "role",
+					);
+					if(defined ($span_r)) {
+						$span_role = $span_r->as_text;
+					} else {
+						$span_role = "undef|past-role";
+					}
+				if ($span_org =~ m/.*($keyword).*/i) {
+					my $entry="$span_first,$span_last,$span_org,$span_role";
+					push (@humint_targets, $entry);
+				}
+				
+				my $random_number_2 = int(rand($range));
+				sleep($random_number_2);
+
+				} else {
+        		print ("[*] ERROR. Could not retrieve target data for " . $uniq_target_url . " . Moving on. \n");
+        	}	#close if/then for mech success
+        }	#close if/then for eva statement error catching
+	}	#close for loop 
 
 	$humint_targets = scalar @humint_targets;
 	print "[*] " . $humint_targets . " suitable human targets found to date \n";
@@ -267,123 +302,140 @@ for my $uniq_page_url (@uniq_page_url) {
 	my $results_page=$1;						#assign the identifier to $results_page	variable.
 	
 	print "[*] Retrieving target URLs from results page " . $results_page . " \n";		#retrieve and verify content
-	$mech->get( $uniq_page_url );													
-	if ($mech->success) {
-
-        my $content = $mech->response()->decoded_content();
-        print "[*] Successful. Content retrieved \n";
+	my $connection_5 = eval {
+		$mech->get( $uniq_page_url );
+		1
+		};
+	
+	if (! $connection_5 ) {
+    	print "[*] ERROR. Unable to retrieve results from page $results_page. Moving on. \n";
+		} else {
+			if ($mech->success) {
+		        my $content = $mech->response()->decoded_content();
+		        print "[*] Successful. Content retrieved \n";
 
 ## parse out links to people *and* links to additional results pages
     
-    	my @target_links_i = $mech->find_all_links( url_regex => qr/\/p\/[a-zA-Z\+].*\/normal\/.*/);	#target individuals
-    	my @page_links_i = $mech->find_all_links( url_regex => qr/&p=.*$/);								#results pages
-    
-    	my @target_url_i; 																				#create absolute urls
-		for my $target_link_i (@target_links_i) {
-			push (@target_url_i, $target_link_i->url_abs);
-		}
-		
-		my @page_url_i; 																				#create absolute urls
-		for my $page_link_i (@page_links_i) {
-			push (@page_url_i, $page_link_i->url_abs);
-		}
-	
-		my @uniq_target_url_i = uniq(@target_url_i);													#remove duplicates from target array
-		print "[*] " . scalar @uniq_target_url_i . " Links to human targets found \n";
-	
-		my @uniq_page_url_i = uniq(@page_url_i);														#remove duplicates from additional pages of results
-		my $new_page_count=0;																			#this is a counter to check for new results pages
-		for my $page_url_i (@uniq_page_url_i) {															#for each result page extract the identifier
-			$page_url_i =~ m/(\d+)$/;
-			my $harvested_identifier=$1;																#assign the identifier to a variable
-			if ($harvested_identifier > $current_count) {												#look to see if this is a "new" results page
-				push (@uniq_page_url, $page_url_i);														#if so push it to the array of results pages
-				++$new_page_count;																		#increment the counter	
-			}
-		}
-		print "[*] " . $new_page_count . " Links to additonal results pages found \n";					#print the total number of new pages found
-		print "\n";
-	
-		##parse out the targets on the current page
-		print "[*] Harvesting human target data with sleep times between 0 and " . $range . " seconds between records \n";
-		print "\n";
-			for my $uniq_target_url_i (@uniq_target_url_i) {
-				$mech->get( $uniq_target_url_i );
-				if ($mech->success) {
-
-					my $scraping_i = $mech->content;		#response()->decoded_content();
+		    	my @target_links_i = $mech->find_all_links( url_regex => qr/\/p\/[a-zA-Z\+].*\/normal\/.*/);	#target individuals
+		    	my @page_links_i = $mech->find_all_links( url_regex => qr/&p=.*$/);								#results pages
+		    
+		    	my @target_url_i; 																				#create absolute urls
+				for my $target_link_i (@target_links_i) {
+					push (@target_url_i, $target_link_i->url_abs);
+				}
 				
-					#create trees to walk the content of the target pages 
-					my $tree_iter_i = HTML::Tree->new(); #create new tree object
+				my @page_url_i; 																				#create absolute urls
+				for my $page_link_i (@page_links_i) {
+					push (@page_url_i, $page_link_i->url_abs);
+				}
 			
-					$tree_iter_i->parse($scraping_i);		#populate tree object with parsed content
-				
-					my $span_first_i;
-					my ($span_f_i) = $tree_iter_i->look_down(	#extract firstname and print as text
-						_tag => "span",
-						class => "given-name firstname",
-						);
-					if(length($span_f_i) != 0) {
-						$span_first_i = $span_f_i->as_text;
-					} else {
-						$span_first_i = "undef";
-					}
-				
-					my $span_last_i;
-					my ($span_l_i) = $tree_iter_i->look_down(	#extract lastname and print as text
-						_tag => "span",
-						class => "family-name lastname",
-						);
-					if(length($span_l_i) != 0) {
-						$span_last_i = $span_l_i->as_text;
-					} else {
-						$span_last_i = "undef";
-					}
-
-					my $span_org_i;
-					my ($span_o_i) = $tree_iter_i->look_down(	#extract organization and print as text
-						_tag => "span",
-						class => "org",
-						);
-					#if(length($span_o_i) != 0) {
-					if(defined ($span_o_i)) {
-						$span_org_i = $span_o_i->as_text;
-					} else {
-						$span_org_i = "undef|past-employer";
-					}
-				
-					my $span_role_i;
-					my ($span_r_i) = $tree_iter_i->look_down(	#extract role and print as text
-						_tag => "span",
-						class => "role",
-						);
-					if(defined ($span_r_i)) {	
-					#if(length($span_r_i) != 0) {
-						$span_role_i = $span_r_i->as_text;
-					} else {
-						$span_role_i = "undef|past-role";
-					}
-					if ($span_org_i =~ m/.*($keyword).*/i) {
-						my $entry="$span_first_i,$span_last_i,$span_org_i,$span_role_i";
-						push (@humint_targets, $entry);
-					}
+				my @uniq_target_url_i = uniq(@target_url_i);													#remove duplicates from target array
+				print "[*] " . scalar @uniq_target_url_i . " Links to human targets found \n";
 			
-					my $random_number_2 = int(rand($range));
-					sleep($random_number_2);
-				
-					} else {
-	        			print ("[*] ERROR. Could not retrieve " . $uniq_target_url_i . " Moving on. \n");
-	    		}	
-			}
-	
-		$humint_targets = scalar @humint_targets;
-		print "[*] " . $humint_targets . " suitable human targets found to date \n";
-		print "\n";
+				my @uniq_page_url_i = uniq(@page_url_i);														#remove duplicates from additional pages of results
+				my $new_page_count=0;																			#this is a counter to check for new results pages
+				for my $page_url_i (@uniq_page_url_i) {															#for each result page extract the identifier
+					$page_url_i =~ m/(\d+)$/;
+					my $harvested_identifier=$1;																#assign the identifier to a variable
+					if ($harvested_identifier > $current_count) {												#look to see if this is a "new" results page
+						push (@uniq_page_url, $page_url_i);														#if so push it to the array of results pages
+						++$new_page_count;																		#increment the counter	
+					}
+				}
+				print "[*] " . $new_page_count . " Links to additonal results pages found \n";					#print the total number of new pages found
+				print "\n";
+			
+				##parse out the targets on the current page
+				print "[*] Harvesting human target data with sleep times between 0 and " . $range . " seconds between records \n";
+				print "\n";
+					for my $uniq_target_url_i (@uniq_target_url_i) {
+						my $connection_6 = eval {
+							$mech->get( $uniq_target_url_i );
+							1
+						};
 
-		} else {
-        	print ("[*] ERROR. Could not retrieve results page " . $uniq_page_url . " Moving on. \n");
-    }
-}
+						if (! $connection_6 ) {
+    						print "[*] ERROR. Unable to retrieve target data for $uniq_target_url_i. Moving on. \n";
+						} else {
+
+							if ($mech->success) {
+
+								my $scraping_i = $mech->content;		#response()->decoded_content();
+							
+								#create trees to walk the content of the target pages 
+								my $tree_iter_i = HTML::Tree->new(); #create new tree object
+						
+								$tree_iter_i->parse($scraping_i);		#populate tree object with parsed content
+							
+								my $span_first_i;
+								my ($span_f_i) = $tree_iter_i->look_down(	#extract firstname and print as text
+									_tag => "span",
+									class => "given-name firstname",
+									);
+								if(length($span_f_i) != 0) {
+									$span_first_i = $span_f_i->as_text;
+								} else {
+									$span_first_i = "undef";
+								}
+							
+								my $span_last_i;
+								my ($span_l_i) = $tree_iter_i->look_down(	#extract lastname and print as text
+									_tag => "span",
+									class => "family-name lastname",
+									);
+								if(length($span_l_i) != 0) {
+									$span_last_i = $span_l_i->as_text;
+								} else {
+									$span_last_i = "undef";
+								}
+
+								my $span_org_i;
+								my ($span_o_i) = $tree_iter_i->look_down(	#extract organization and print as text
+									_tag => "span",
+									class => "org",
+									);
+								#if(length($span_o_i) != 0) {
+								if(defined ($span_o_i)) {
+									$span_org_i = $span_o_i->as_text;
+								} else {
+									$span_org_i = "undef|past-employer";
+								}
+							
+								my $span_role_i;
+								my ($span_r_i) = $tree_iter_i->look_down(	#extract role and print as text
+									_tag => "span",
+									class => "role",
+									);
+								if(defined ($span_r_i)) {	
+								#if(length($span_r_i) != 0) {
+									$span_role_i = $span_r_i->as_text;
+								} else {
+									$span_role_i = "undef|past-role";
+								}
+								if ($span_org_i =~ m/.*($keyword).*/i) {
+									my $entry="$span_first_i,$span_last_i,$span_org_i,$span_role_i";
+									push (@humint_targets, $entry);
+								}
+						
+								my $random_number_2 = int(rand($range));
+								sleep($random_number_2);
+							
+							} else {
+			        			print ("[*] ERROR. Could not retrieve " . $uniq_target_url_i . " Moving on. \n");
+			    			}	#close if/then for mech success
+			    		}	#close if/then for eval statement catching errors (connection 6)
+					}	#close for loop for iterating through individual targets
+			
+				$humint_targets = scalar @humint_targets;
+				print "[*] " . $humint_targets . " suitable human targets found to date \n";
+				print "\n";
+
+				} else {
+        			print ("[*] ERROR. Could not retrieve results page " . $uniq_page_url . " Moving on. \n");
+    		}	#close if/then for mech success statement
+    }	#close if/then for eval statement catching errors (connection 5)
+}	#close ouer for loop for results pages
+
 print "[*] candidate target list \n";
 print "\n";
 print join("\n", @humint_targets), "\n";
