79b7d5e14087320c5a239be8130c10372cd900a9
phobos <phobos@colossian.netgear.com>
2016-07-30 14:38:43 -0400

    work in progress

--- proxget.py
+++ proxget.py
@@ -34,56 +34,6 @@ import urllib2
 import socket
 import errno
 
-def grab( url ): #retrieves and returns the target resource
-	capturedDocs=[] #initialize array
-	#build request
-	user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
-	request = urllib2.Request(url)
-	##add header
-	request.add_header('User-Agent', user_agent)
-	## create opener(s)
-	proxy_handler = urllib2.ProxyHandler({'http': 'http://:80'})
-	opener = urllib2.build_opener(proxy_handler)
-	##install opener
-	urllib2.install_opener(opener)
-	#make request using the installed opener
-	try:
-		capture = urllib2.urlopen(request, timeout=4)
-	except urllib2.URLError, e:
-		if hasattr(e, 'reason'):
-			print 'Server could not be reached'
-			print 'Reason:',e.reason
-			print
-		elif hasattr(e,code):
-			print 'Server could not handle request'
-			print 'Error code:',e.code
-			print
-		else:
-			print 'Unknown error'
-			print
-		pass
-	except Exception, e:
-		print e
-		print
-		pass
-	else:
-		returnCode=capture.getcode()
-		#examine return code and bucket array
-		if returnCode == 200:
-			capturedDocs.append( url )
-			print 'Captured',url
-			print
-		else:
-			print
-	return capture;
-
-def plunk( docBlob ): #writes the target resource to a file on disk
-	#write to file
-	output=open('test.pdf','wb')
-	output.write(docBlob.read())
-	output.close()
-	return;
-
 def file_ingest( url_list_file ): #parses file containing list of URLs and returns a list
 	#write URLs to a python list
 	with open(url_list_file, 'r') as infile:
@@ -98,14 +48,59 @@ def proxy_ingest( proxy_list_file ): #parses file containing list of proxys and 
 	proxyList = data.splitlines()
 	return proxyList;
 
-def proxy_test ( proxy_list ): #tests each candidate proxy from list and returns a list of hot proxies
-	hotProxy=[] #initialize array
-	for proxy in proxy_list:
-		print 'Testing:', proxy
+def proxy_test ( proxy_candidate): #tests a candidate proxy from list and returns a list of hot proxies
+	#build request
+	returnCode=None
+	user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
+	test_url = "http://www.stackoverflow.com"
+	request = urllib2.Request(test_url)
+	##add header
+	request.add_header('User-Agent', user_agent)
+	## create opener(s)
+	proxy_handler = urllib2.ProxyHandler({'http': proxy_candidate})
+	opener = urllib2.build_opener(proxy_handler)
+	##install opener
+	urllib2.install_opener(opener)
+	#make request
+	try: 
+		testResult = urllib2.urlopen(request, timeout=4)			
+	except urllib2.URLError, e:
+		if hasattr(e, 'reason'):
+			print '\t\tServer could not be reached'
+			print '\t\tReason:',e.reason
+		elif hasattr(e,code):
+			print '\t\tServer could not handle request'
+			print '\t\tError code:',e.code
+		else:
+			print '\t\tUnknown error'
+		pass
+	except Exception, e:
+		print '\t\tUnknown error',e
+		pass
+	else:
+		returnCode=testResult.getcode()
+	#examine return code and bucket array
+	if returnCode == 200:
+		return proxy_candidate;
+	else:
+		return;
+
+def select_proxy ( hot_proxies ):
+	# select candidate from array
+	# remove candidate from array
+	# send modified array back to caller
+	candidate = hot_proxies.pop(0)
+	proxyPool = hot_proxies
+	return candidate,proxyPool;
+
+def grab( url_list, proxy ): #retrieves and returns the target resource
+	capturedDocs=[] #initialize array
+	orphanedDocs=[] #initialize array
+	returnCode=None
+	for url in url_list:
 		#build request
 		user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
-		test_url = "http://www.stackoverflow.com"
-		request = urllib2.Request(test_url)
+		request = urllib2.Request(url)
 		##add header
 		request.add_header('User-Agent', user_agent)
 		## create opener(s)
@@ -113,74 +108,83 @@ def proxy_test ( proxy_list ): #tests each candidate proxy from list and returns
 		opener = urllib2.build_opener(proxy_handler)
 		##install opener
 		urllib2.install_opener(opener)
-		#make request
-		try: 
-			testResult = urllib2.urlopen(request, timeout=4)			
+		#make request using the installed opener
+		try:
+			capture = urllib2.urlopen(request, timeout=4)
 		except urllib2.URLError, e:
 			if hasattr(e, 'reason'):
-				print 'Server could not be reached'
-				print 'Reason:',e.reason
-				print
+				print '\t\tServer could not be reached'
+				print '\tReason:',e.reason
 			elif hasattr(e,code):
-				print 'Server could not handle request'
-				print 'Error code:',e.code
-				print
+				print '\t\tServer could not handle request'
+				print '\t\tError code:',e.code
 			else:
-				print 'Unknown error'
-				print
+				print '\t\tUnknown error'
 			continue
 		except Exception, e:
-			print e
-			print
+			print '\t\tUnknown error',e
 			continue
 		else:
-			returnCode=testResult.getcode()
+			returnCode=capture.getcode()
 			#examine return code and bucket array
 			if returnCode == 200:
-				hotProxy.append( proxy )
-				print 'Proxy is up'
+				capturedDocs.append( url )
+				print 'Captured',url
 				print
 			else:
+				orphanedDocs.append( url )
+				print 'Orphaned',url
 				print
-	return hotProxy;
+	return capture;
 
-#docBlob = grab(candidate_url)
-#plunk(docBlob)
-#bleeb = proxy_ingest(proxy_file)
-#bloob = proxy_test(bleeb)
-#print 'The list of hot proxies includes:'
-#print
-#for line in bloob:
-#	print line
+def plunk( docBlob ): #writes the target resource to a file on disk
+	#write to file
+	output=open('test.pdf','wb')
+	output.write(docBlob.read())
+	output.close()
+	return;
 
-#main flow
+# [*] MAIN
 
-## variable definitions
-
+# variable definitions
 candidate_url = "http://www.paterva.com/MSL.pdf"
 url_file = "urlList.txt"
 proxy_file = "proxyList.txt"
 
-## read in file with URLs and return a python list
-
+# read in file with URLs and return a python list
+print '[*] reading in list of target URLs'
 urlList = file_ingest(url_file)
 l = len(urlList)
-print l
+print '\t... there are %s targets to retrieve' %l
+print
 
-## read in file with proxies and return a python list
-
+# read in file with proxies and return a python list
+print '[*] reading in list of proxies'
 proxyList = proxy_ingest(proxy_file)
 p = len(proxyList)
-print p
+print '\t... there are %s candidate proxies' %p
+print
 
-## test proxies and return a list of hot proxies
+# test proxies
+hotProxy=[] #initialize array
+print '[*] Testing proxies'
+for proxy in proxyList:
+	print '\tTesting %s' %proxy
+	candidate = proxy_test(proxy)
+	#print candidate
+	if candidate:
+		hotProxy.append(candidate)
+		print '\t\t... %s is hot' %candidate
+		print	
+	else:
+		print '\t\t...  no response'
+		print
 
-hotproxyList = proxy_test(proxyList)
-hp = len(hotproxyList)
-print hp
+# select proxy
+print '[*] Selecting proxies'
+proxy, proxyPool = select_proxy(hotProxy)
+print '\tSelected %s' %proxy
+q = len(proxyPool)
+print '\tThere are %s remaining proxies in reserve' %q
 
-## for each URL call the grab function and pass one URL and one hot proxy
-
-## the grab function needs to return the binary blob and a file name
-
-## for each capture returned call the plunk function with the binary blob and filename and write it to disk
+# retrieve documents
